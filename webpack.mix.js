49eb384ed428e256b4e1e914f739097c7b633c45
Cargalher <cargalher@gmail.com>
2021-06-15 18:07:56 +0200

    cards dimensions

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,8 @@ let mix = require('laravel-mix');
 mix
 .js('src/app.js', 'js')
 .sass('src/app.scss', 'css')
-.copyDirectory('src/img', 'dist/img')
-.setPublicPath('dist')
 .options({
     processCssUrls: false // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
-  });
+  })
+.copyDirectory('src/img', 'dist/img')
+.setPublicPath('dist');
