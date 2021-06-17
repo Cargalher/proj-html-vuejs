ad11c310d4804a5b3ea2579f5d25fc50aef7d3f4
Cargalher <cargalher@gmail.com>
2021-06-17 10:12:16 +0200

    festival list in section5 rendered with v-for

--- dist/js/app.js
+++ dist/js/app.js
@@ -24,6 +24,17 @@ var app = new Vue({
       text: "Albums"
     }, {
       text: "Fans"
+    }],
+    festivals: [{
+      date: "17/08/2020 gem fesival 2020 anakalia, georgia"
+    }, {
+      date: "24/09/2020 groovefest dominical republic"
+    }, {
+      date: "31/10/2020 oasis festival 2020 marrakech, morocco"
+    }, {
+      date: "07/11/2020 moga festival - essaouria, morocco"
+    }, {
+      date: "10/12/2020 envision festival - uvita, costa rica"
     }]
   },
   methods: function methods() {},
--- src/app.js
+++ src/app.js
@@ -4,30 +4,14 @@ const app = new Vue ({
         logo:'./dist/img/avada-music-logo.png',
         isShowDrop: false,
         menuElements:[
-            {
-                text: "Home",
-                
-            },
-            {
-                text: "Meet the brand",
-                                 
-            },
-            {
-                text: "Live Dates"
-            },
-            {
-                text: "Latest News",
-              
-            },
-            {
-                text: "Albums",
-                
-            },
-            {
-
-                text: "Fans"
-            },
+            {text: "Home"}, {text: "Meet the brand"}, {text: "Live Dates"}, {text: "Latest News"}, {text: "Albums"}, {text: "Fans"},
         ],
+        festivals:[
+            
+            {date: "17/08/2020 gem fesival 2020 anakalia, georgia"}, {date:"24/09/2020 groovefest dominical republic"}, {date:"31/10/2020 oasis festival 2020 marrakech, morocco"}, {date:"07/11/2020 moga festival - essaouria, morocco"}, {date:"10/12/2020 envision festival - uvita, costa rica"},
+        ],
+       
+        
     },
     methods(){
         
@@ -39,3 +23,4 @@ const app = new Vue ({
     }
 
 })
+
